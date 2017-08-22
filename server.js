9e1919bb4d4c9c27a42205f48f69da37734cab4c
creightons <creightons26@gmail.com>
2017-08-22 18:26:25 -0400

    Iframe link is generated randomly, can be shuffled with button click

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ server.use((req, res, next) => {
 });
 
 server.get('/', (req, res) => res.sendFile(getFilePath('./index.html')));
-server.get('/iframed-page', (req, res) => res.sendFile(getFilePath('./iframed-page.html')));
+server.get('/iframe-1', (req, res) => res.sendFile(getFilePath('./iframe-1.html')));
+server.get('/iframe-2', (req, res) => res.sendFile(getFilePath('./iframe-2.html')));
 
 server.listen(3000, () => console.log('live on port 3000'));
